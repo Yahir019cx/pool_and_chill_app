cbbc61b820a11958e60d28001a59c9ffc86ebf1a
Yahir019cx <pyahirsvds@gmail.com>
2026-02-25 19:56:13 -0600

    Configure Firebase correctly with flutterfire

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart' hide ChangeNotifierProvider;
@@ -15,7 +16,9 @@ import 'package:pool_and_chill_app/app/auth_gate.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   Stripe.publishableKey =
       'pk_test_51Suiwb1n9zmFHAn7FUZNOJZYdpzndQxY3CLtuEBTxdOg98v2kLbR38DLZP7EYUiHBMigeoTKwxLo6Uuhna3fNYXv00zgzZZczG';
