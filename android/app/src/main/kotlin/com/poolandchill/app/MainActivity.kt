6bda2b72ac7286efcace034de40152340c0e9a53
Yahir019cx <pyahirsvds@gmail.com>
2026-03-01 14:20:55 -0600

    fix: android

--- android/app/src/main/kotlin/com/poolandchill/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/poolandchill/app/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.poolandchill.app
 
 import android.os.Bundle
-import androidx.activity.EdgeToEdge
+import androidx.activity.enableEdgeToEdge
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity : FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        EdgeToEdge.enable(this)
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
     }
 }
